55cdca5281b8cfc500484c9d3c6cd953686acf73
zhangyaoqiang <zhangyaoqiang001@outlook.com>
2017-06-20 09:55:13 +0800

    chengxu1
    create chengxu

---
+++ cgi-stu/stu/add.c
@@ -0,0 +1,109 @@
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <mysql/mysql.h>
+#include "cgic.h"
+
+char * headname = "head.html";
+char * footname = "footer.html";
+
+int cgiMain()
+{
+
+	FILE * fd;
+
+	char name[32] = "\0";
+	char age[16] = "\0";
+	char stuId[32] = "\0";
+	int status = 0;
+	char ch;
+
+	fprintf(cgiOut, "Content-type:text/html;charset=utf-8\n\n");
+	if(!(fd = fopen(headname, "r"))){
+		fprintf(cgiOut, "Cannot open file, %s\n", headname);
+		return -1;
+	}
+	ch = fgetc(fd);
+
+	while(ch != EOF){
+		fprintf(cgiOut, "%c", ch);
+		ch = fgetc(fd);
+	}
+	fclose(fd);
+
+	status = cgiFormString("name",  name, 32);
+	if (status != cgiFormSuccess)
+	{
+		fprintf(cgiOut, "get name error!\n");
+		return 1;
+	}
+
+	status = cgiFormString("age",  age, 16);
+	if (status != cgiFormSuccess)
+	{
+		fprintf(cgiOut, "get age error!\n");
+		return 1;
+	}
+
+	status = cgiFormString("stuId",  stuId, 32);
+	if (status != cgiFormSuccess)
+	{
+		fprintf(cgiOut, "get stuId error!\n");
+		return 1;
+	}
+
+	//fprintf(cgiOut, "name = %s, age = %s, stuId = %s\n", name, age, stuId);
+
+	int ret;
+	char sql[128] = "\0";
+	MYSQL *db;
+
+	//初始化
+	db = mysql_init(NULL);
+	if (db == NULL)
+	{
+		fprintf(cgiOut,"mysql_init fail:%s\n", mysql_error(db));
+		return -1;
+	}
+
+	//连接数据库
+	db = mysql_real_connect(db, "127.0.0.1", "root", "123456", "stu",  3306, NULL, 0);
+	if (db == NULL)
+	{
+		fprintf(cgiOut,"mysql_real_connect fail:%s\n", mysql_error(db));
+		mysql_close(db);
+		return -1;
+	}
+
+
+
+	strcpy(sql, "create table stu(id int not null primary key, name varchar(20) not null, age int not null)");
+	if ((ret = mysql_real_query(db, sql, strlen(sql) + 1)) != 0)
+	{
+		if (ret != 1)
+		{
+			fprintf(cgiOut,"mysql_real_query fail:%s\n", mysql_error(db));
+			mysql_close(db);
+			return -1;
+		}
+	}
+
+
+
+	sprintf(sql, "insert into stu values(%d, '%s', %d)", atoi(stuId), name, atoi(age));
+	if (mysql_real_query(db, sql, strlen(sql) + 1) != 0)
+	{
+		fprintf(cgiOut, "%s\n", mysql_error(db));
+		mysql_close(db);
+		return -1;
+	}
+
+	fprintf(cgiOut, "add student ok!\n");
+	mysql_close(db);
+	return 0;
+}
+
+    Contact GitHub API Training Shop Blog About
+
+    © 2017 GitHub, Inc. Terms Privacy Security Status Help
